6365aaef21bef2ae51f08ef516342b07d527dd2b
Dudu Montezuma <carlos15montezuma@gmail.com>
2022-02-24 23:07:11 -0300

    Adicionando arquivos
    Adicionei os arquivos ao repositório

---
+++ game cap1/forca.py
@@ -0,0 +1,13 @@
+
+def jogar():
+    print('*' * 32)
+    print('BEM VINDO AO JOGO DE FORCA')
+    print('*' * 32)
+
+
+    print('*' * 32)
+    print('Fim do jogo')
+    print('*' * 32)
+
+if(__name__ == "__main__"):
+    jogar()
